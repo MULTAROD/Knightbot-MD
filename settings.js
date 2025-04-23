47b7242ec2d441c4d4b48cc52c8502dd38f66219
MULTAROD <sanatnivicharrd@gmail.com>
2025-04-23 15:39:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight md",
-  botOwner: 'knight md', // Your name
+  botName: "Knightbotmd",
+  botOwner: 'knightbotmd', // Your name
   ownerNumber: '919625470013', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
